5469f958394573fe6756b0241401723d4eb1e4eb
Ko Nishiura <myoko0905@outlook.jp>
2021-08-05 11:20:57 +0900

    day2 commit

---
+++ components/Footer.js
@@ -0,0 +1,34 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 space-y-10 px-32 py-14 bg-gray-100 text-gray-600 ">
+      <div className="space-y-4 text-xs text-gray-800 row-start-1">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb work</p>
+        <p>News</p>
+        <p>Investore</p>
+        <p>Airbnb Olus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800 row-start-1">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This is not a real site</p>
+        <p>Its a pretty awesome clone</p>
+        <p></p>
+        <p></p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>XIPU</p>
+        <p>Present</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Hepl Center</p>
+        <p>Trust & Safety</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
