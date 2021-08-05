4e07aa5e4fa77947be1e07f20da1d2cce5f4de29
Ko Nishiura <myoko0905@outlook.jp>
2021-08-05 14:25:58 +0900

    git push

--- components/Footer.js
+++ components/Footer.js
@@ -1,21 +1,19 @@
 function Footer() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-4 space-y-10 px-32 py-14 bg-gray-100 text-gray-600 ">
-      <div className="space-y-4 text-xs text-gray-800 row-start-1">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 py-16 px-10 bg-gray-100 text-gray-600 ">
+      <div className="space-y-4 text-xs text-gray-800 ">
         <h5 className="font-bold">ABOUT</h5>
         <p>How Airbnb work</p>
         <p>News</p>
         <p>Investore</p>
-        <p>Airbnb Olus</p>
+
         <p>Airbnb Luxe</p>
       </div>
-      <div className="space-y-4 text-xs text-gray-800 row-start-1">
+      <div className="space-y-4 text-xs text-gray-800 ">
         <h5 className="font-bold">COMMUNITY</h5>
         <p>Accessibility</p>
         <p>This is not a real site</p>
         <p>Its a pretty awesome clone</p>
-        <p></p>
-        <p></p>
       </div>
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold">HOST</h5>
