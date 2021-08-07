1539da68ea4fea443d7d4bc3f89f51874eb08e6b
Ko Nishiura <myoko0905@outlook.jp>
2021-08-07 17:20:01 +0900

    day3

---
+++ components/Map.js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import getCenter from "geolib/es/getCenter";
+
+function Map({ searchResults }) {
+  const [selectedLocation, setSelectedLocation] = useState({});
+
+  const cordinates = searchResults.map((result) => ({
+    longitude: result.long,
+    latitude: result.lat,
+  }));
+
+  const center = getCenter(cordinates);
+
+  const [viewport, setViewport] = useState({
+    width: "100%",
+    height: "100%",
+    latitude: center.latitude,
+    longitude: center.longitude,
+    zoom: 11,
+  });
+
+  return (
+    <ReactMapGL
+      mapStyle="mapbox://styles/konishiura/cks1dgx9a359m17mi9qv0klju"
+      mapboxApiAccessToken={process.env.mapbox_key}
+      {...viewport}
+      onViewportChange={(nextViewport) => setViewport(nextViewport)}
+    >
+      {searchResults.map((result) => (
+        <div key={result.long}>
+          <Marker longitude={result.long} latitude={result.lat}>
+            <p
+              role="img"
+              onClick={() => setSelectedLocation(result)}
+              className="animate-bounce cursor-pointer text-2xl"
+              aria-label="round_pushpin"
+            >
+              📍
+            </p>
+          </Marker>
+
+          {selectedLocation.long === result.long ? (
+            <Popup
+              onClose={() => setSelectedLocation({})}
+              closeOnClick={true}
+              latitude={result.lat}
+              longitude={result.long}
+            >
+              {result.title}
+            </Popup>
+          ) : (
+            false
+          )}
+        </div>
+      ))}
+    </ReactMapGL>
+  );
+}
+
+export default Map;
--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
+  env: {
+    mapbox_key:
+      "pk.eyJ1Ijoia29uaXNoaXVyYSIsImEiOiJja3MweTZvYTkwOWd1MnVtdGFhc3FvbXBqIn0.6nGTSHh3AeikQc1wzkfyOA",
+  },
 };
